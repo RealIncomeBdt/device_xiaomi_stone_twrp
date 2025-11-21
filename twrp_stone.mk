1a6b9c9d12690becda66202f7730bc0b454b7aef
RealIncomeBdt <165435128+RealIncomeBdt@users.noreply.github.com>
2025-11-22 01:10:03 +0600

    Update twrp_stone.mk

--- twrp_stone.mk
+++ twrp_stone.mk
@@ -5,21 +5,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Base product (64-bit)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# TWRP common configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from stone device
+# Device configuration
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
 PRODUCT_DEVICE := stone
 PRODUCT_NAME := twrp_stone
+PRODUCT_RELEASE_NAME := stone
+
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 12 5G
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
